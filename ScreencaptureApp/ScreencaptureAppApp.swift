128717f368050e0d61994c9fca9036434b1944a7
Monsef Eledrisse <zaone035@gmail.com>
2025-03-11 14:58:22 +0200

    Initial Commit

---
+++ ScreencaptureApp/ScreencaptureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreencaptureAppApp.swift
+//  ScreencaptureApp
+//
+//  Created by Monsef Eledrisse on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreencaptureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreencaptureAppTests/ScreencaptureAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreencaptureAppTests.swift
+//  ScreencaptureAppTests
+//
+//  Created by Monsef Eledrisse on 3/11/25.
+//
+
+import Testing
+@testable import ScreencaptureApp
+
+struct ScreencaptureAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
