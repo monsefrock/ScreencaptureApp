66fc89dbfbe44e8a80c9044c3b6e539f31fc36e6
Monsef Eledrisse <zaone035@gmail.com>
2025-03-11 15:59:48 +0200

    Initial commit

--- ScreencaptureApp/ContentView.swift
+++ ScreencaptureApp/ContentView.swift
@@ -8,16 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var vm = ScreencaptureViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 200, maximum: 300))]) {
+                    ForEach(vm.images, id: \.self){ image in
+                        Image(nsImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .draggable(image)
+                    }
+                }
+            }
+            
+
+            HStack {
+                Button("Take Area Screenshot") {
+                    
+                    vm.takeScreenShot(type: .area)
+                }
+                
+                Button("Take Full Screen Screenshot") {
+                    vm.takeScreenShot(type: .fullscreen)
+                }
+                
+                Button("Take Window Screenshot")
+                {
+                    vm.takeScreenShot(type: .window)
+                }
+            }
+            
         }
         .padding()
     }
+    
+    
 }
+
+
 
 #Preview {
     ContentView()
---
+++ ScreencaptureApp/ScreencaptureViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  ScreencaptureViewModel.swift
+//  ScreencaptureApp
+//
+//  Created by Monsef Eledrisse on 3/11/25.
+//
+
+import SwiftUI
+
+class ScreencaptureViewModel: ObservableObject {
+    
+    @Published var images = [NSImage]()
+    
+    enum ScreenshotType {
+        
+        case fullscreen
+        case window
+        case area
+        
+        var processArguments: [String] {
+            switch self {
+            case .fullscreen:
+                return ["-c"]
+            case .window:
+                return ["-cw"]
+            case .area:
+                return ["-cs"]
+            }
+        }
+    }
+    
+    
+    public func takeScreenShot(type: ScreenshotType) {
+        
+        let task = Process()
+        task.executableURL = URL(fileURLWithPath: "/usr/sbin/screencapture")
+        
+        task.arguments = type.processArguments
+        
+        do {
+            try task.run()
+            task.waitUntilExit()
+            
+            getCapturedImage()
+        } catch {
+            print("Error running task: \(error)")
+        }
+    }
+    
+    private func getCapturedImage() {
+        guard NSPasteboard.general.canReadItem(withDataConformingToTypes: NSImage.imageTypes) else {
+            return
+        }
+        
+        guard let image = NSImage(pasteboard: .general) else {
+            return
+        }
+        
+        self.images.append(image)
+    }
+    
+}
